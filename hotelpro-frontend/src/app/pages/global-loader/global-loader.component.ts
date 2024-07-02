af1d647137d1f98b0e25932f54420edff8072f12
Ravi Parmar <ravi.parmar@simulas.in>
2024-07-02 18:44:32 +0000

    global lodaer update

--- hotelpro-frontend/src/app/pages/global-loader/global-loader.component.ts
+++ hotelpro-frontend/src/app/pages/global-loader/global-loader.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { LoaderService } from '../../core/services/loader.service';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { CommonModule } from '@angular/common';
-import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-global-loader',
@@ -15,12 +14,4 @@ export class GlobalLoaderComponent {
   loading$ = this.loaderService.loading$;
 
   constructor(private loaderService: LoaderService) {}
-
-  isUserLogin() {
-    if (localStorage.getItem(environment.storageKey)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 }
