483dd79c298351b3861727d7243150a129ed2395
Ravi Parmar <ravi.parmar@simulas.in>
2024-07-03 18:06:30 +0000

    web worker added in frontend

--- hotelpro-frontend/src/app/pages/property-setup/add-update-property/add-update-property.component.ts
+++ hotelpro-frontend/src/app/pages/property-setup/add-update-property/add-update-property.component.ts
@@ -1,26 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { APIConstant } from '../../../core/constants/APIConstant';
 import { AuthService } from '../../../core/services/auth.service';
-import { CrudService } from '../../../core/services/crud.service';
 
 @Component({
   selector: 'app-add-update-property',
   templateUrl: './add-update-property.component.html',
-  styleUrl: './add-update-property.component.css',
+  styleUrls: ['./add-update-property.component.css'],
 })
-export class AddUpdatePropertyComponent implements OnInit {
+export class AddUpdatePropertyComponent implements OnInit, OnDestroy {
   userInfo: any;
   userForm!: FormGroup;
+  private worker!: Worker;
+  public workerResponse: any;
 
-  constructor(
-    private authService: AuthService,
-    private crudService: CrudService,
-    private fb: FormBuilder
-  ) {}
+  constructor(private authService: AuthService, private fb: FormBuilder) {}
+
   ngOnInit(): void {
-    const u = this.authService.getUserInfo();
-    this.userInfo = u?.user;
+    this.userInfo = this.authService.getUserInfo()?.user;
 
     this.userForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
@@ -35,10 +31,47 @@ export class AddUpdatePropertyComponent implements OnInit {
       this.userForm.markAllAsTouched(); // Mark all controls as touched to display validation errors
     }
   }
+
   rr() {
     this.userForm.reset({
       username: 'ravi',
       password: 'sss',
     });
+  }
+
+  ngOnDestroy() {
+    if (this.worker) {
+      this.worker.terminate();
+    }
+  }
+  callWebWorker() {
+    if (typeof Worker !== 'undefined') {
+      // Create a new web worker
+      this.worker = new Worker(
+        new URL('../../../core/workers/api-caller.worker', import.meta.url),
+        { type: 'module' }
+      );
+
+      // Listen for messages from the worker
+      this.worker.onmessage = ({ data }) => {
+        if (data.status === 'success') {
+          this.workerResponse = data.data;
+        } else {
+          console.error(`Worker error: ${data.message}`);
+        }
+      };
+
+      // Post a message to the worker to make an API call
+      this.worker.postMessage({
+        url: 'https://fakestoreapi.com/products/', // Replace with your API URL
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          // Add other headers as needed
+        },
+      });
+    } else {
+      console.warn('Web Workers are not supported in this environment.');
+    }
   }
 }
