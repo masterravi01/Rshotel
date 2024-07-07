d25bbe311dce5f5ebbaa13dbfadda9911a91018f
Hiren Khunt <hirenkhunt5960@gmail.com>
2024-07-07 23:11:01 +0530

    Implement backend file structure

--- hotelpro-backend/routes/user.route.js
+++ hotelpro-backend/routes/user.route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const user = require("../controllers/auth/user.controllers");
+const user = require("../controllers/auth/auth.controllers");
 
-const { verifyJWT } = require("../middleware/auth.middlewares")
+const { verifyJWT } = require("../middleware/auth.middlewares");
 
 router.post("/register", user.registerUser);
 router.post("/login", user.loginUser);
@@ -13,6 +13,5 @@ router.post("/reset-password/:resetToken", user.resetForgottenPassword);
 router.post("/logout", verifyJWT, user.logoutUser);
 router.post("/change-password", verifyJWT, user.changeCurrentPassword);
 router.post("/current-user", verifyJWT, user.getCurrentUser);
-
 
 module.exports = router;
