55f069c458a015f603c1137dd439ce5afcf702f1
Hiren Khunt <akash.suvagiya@simulas.in>
2024-07-08 01:39:02 -0700

    File name changes

--- hotelpro-backend/routes/index.route.js
+++ hotelpro-backend/routes/index.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Router } = require("express");
 const { verifyJWT } = require("../middleware/auth.middlewares")
-const user = require("./user.route");
+const user = require("./auth.route");
 const admin = require("./admin.route");
 
 router.use("/user", user);
