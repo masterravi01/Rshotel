b079cddc3a28cb927bf72ae121e7a651755737b9
Ravi Parmar <ravi.parmar@simulas.in>
2024-09-10 10:21:43 +0000

    guest folio style added

--- hotelpro-frontend/src/app/pages/reservation-collection/guest-folio/guest-folio.component.ts
+++ hotelpro-frontend/src/app/pages/reservation-collection/guest-folio/guest-folio.component.ts
@@ -10,11 +10,18 @@ import {
   DatePipe,
   JsonPipe,
 } from '@angular/common';
+import { NgImageSliderModule } from 'ng-image-slider-v17';
 
 @Component({
   selector: 'app-guest-folio',
   standalone: true,
-  imports: [JsonPipe, DatePipe, CurrencyPipe, CommonModule],
+  imports: [
+    JsonPipe,
+    DatePipe,
+    CurrencyPipe,
+    CommonModule,
+    NgImageSliderModule,
+  ],
   templateUrl: './guest-folio.component.html',
   styleUrl: './guest-folio.component.css',
 })
@@ -22,6 +29,27 @@ export class GuestFolioComponent implements OnInit {
   groupDetails: any;
   propertyUnitId: string | null = '';
   groupId: string | null = '';
+  imageObject = [
+    {
+      image:
+        'https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      thumbImage:
+        'https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      image:
+        'https://media.istockphoto.com/id/1390233984/photo/modern-luxury-bedroom.jpg?s=612x612&w=0&k=20&c=po91poqYoQTbHUpO1LD1HcxCFZVpRG-loAMWZT7YRe4=',
+      thumbImage:
+        'https://media.istockphoto.com/id/1390233984/photo/modern-luxury-bedroom.jpg?s=612x612&w=0&k=20&c=po91poqYoQTbHUpO1LD1HcxCFZVpRG-loAMWZT7YRe4=',
+    },
+    {
+      image:
+        'https://assets.architecturaldigest.in/photos/60084dd6cce5700439e12bf7/16:9/w_2560%2Cc_limit/modern-living-room-decor-1366x768.jpg',
+      thumbImage:
+        'https://assets.architecturaldigest.in/photos/60084dd6cce5700439e12bf7/16:9/w_2560%2Cc_limit/modern-living-room-decor-1366x768.jpg',
+      title: 'Example with title.',
+    },
+  ];
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
