4e55b8caaa6e927e4006ad0bc0a65d763f36beb4
Ravi Parmar <ravi.parmar@simulas.in>
2024-10-22 05:44:22 +0000

    change in index js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,60 @@
 import "./config.js";
-import { app } from "./app.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorHandler } from "./middlewares/error.middlewares.js";
+import path from "path";
+
+const app = express();
+
+const corsOptions = {
+  origin:
+    process.env.CORS_ORIGIN === "*"
+      ? "*" // This might give CORS error for some origins due to credentials set to true
+      : process.env.CORS_ORIGIN?.split(","), // For multiple cors origin for production.
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: false, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+import { fileURLToPath } from "url"; // Import fileURLToPath for ES modules
+
+const __filename = fileURLToPath(import.meta.url); // Get the current module's URL
+const __dirname = path.dirname(__filename); // Get the directory name
+//routes import
+import indexRouter from "./routes/index.routes.js";
+
+//routes declaration
+app.use("/api/v1", indexRouter);
+const distDir = path.join(__dirname, "dist", "hotelpro-frontend", "browser"); // Use the new __dirname
+app.use(express.static(distDir));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(distDir, "index.html"));
+});
+// Handling preflight requests
+// preflight requests sent by the browser to determine whether the actual request (e.g., a GET or POST request) is safe to send.
+app.options(
+  "*",
+  cors({
+    origin: true,
+
+    credentials: true,
+  })
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(path.resolve(), "src", "views"));
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 8000;
 
-import connectDB from "./db/index.js";
-import { logger } from "./logger/winston.logger.js";
-
-// connectDB()
-//   .then(() => {
-//     app.on("error", (error) => {
-//       logger.error("ERROR:", error);
-//       throw error;
-//     });
-//     app.listen(port, () => {
-//       logger.info(`App is listening on port ${port}`);
-//     });
-//   })
-//   .catch((err) => {
-//     logger.error("mongodb connection failed !!!", err);
-//   });
-
 app.listen(port, () => {
-  logger.info(`App is listening on port ${port}`);
+  console.log(`App is listening on port ${port}`);
 });
 app.get("/zz", (req, res) => {
   res.send("Hello World!");
